afa2735a5ae0c82dc5049753d765b1766b64175f
Jazenn <jason.griffioen@student.hu.nl>
2023-06-05 10:59:16 +0200

    Implemented sublasses into graph.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include "include\graph.hpp"
 #include "include\node.hpp"
 #include "include\edge.hpp"
+#include "include\autorit.hpp"
+#include "include\treinrit.hpp"
+#include "include\vlucht.hpp"
 
 int main(){
     Node nodeA = Node("A");
@@ -16,35 +19,35 @@ int main(){
     Node nodeG = Node("G");
     Node nodeH = Node("H");
 
-    Edge edge1 = Edge(&nodeA, &nodeB, 5);
-    Edge edge2 = Edge(&nodeA, &nodeC, 4);
-    Edge edge3 = Edge(&nodeA, &nodeD, 2);
-    Edge edge4 = Edge(&nodeB, &nodeE, 1);
-    Edge edge5 = Edge(&nodeC, &nodeE, 1);
-    Edge edge6 = Edge(&nodeC, &nodeF, 5);
-    Edge edge7 = Edge(&nodeC, &nodeD, 6);
-    Edge edge8 = Edge(&nodeD, &nodeC, 7);
-    Edge edge9 = Edge(&nodeD, &nodeF, 3);
-    Edge edge10 = Edge(&nodeE, &nodeC, 2);
-    Edge edge11 = Edge(&nodeE, &nodeH, 2);
-    Edge edge12 = Edge(&nodeF, &nodeD, 4);
-    Edge edge13 = Edge(&nodeF, &nodeG, 5);
-    Edge edge14 = Edge(&nodeH, &nodeE, 6);
-    Edge edge15 = Edge(&nodeH, &nodeG, 5);
-    Edge edge16 = Edge(&nodeG, &nodeH, 1);
+    Autorit autorit1 = Autorit(&nodeA, &nodeB, 5);
+    Autorit autorit2 = Autorit(&nodeA, &nodeC, 4);
+    Treinrit treinrit1 = Treinrit(&nodeA, &nodeD, 2);
+    Treinrit treinrit2 = Treinrit(&nodeB, &nodeE, 1);
+    Vlucht vlucht1 = Vlucht(&nodeC, &nodeE, 1);
+    Autorit autorit3 = Autorit(&nodeC, &nodeF, 5);
+    Autorit autorit4 = Autorit(&nodeC, &nodeD, 6);
+    Vlucht vlucht2 = Vlucht(&nodeD, &nodeC, 7);
+    Vlucht vlucht3 = Vlucht(&nodeD, &nodeF, 3);
+    Vlucht vlucht4 = Vlucht(&nodeE, &nodeC, 2);
+    Treinrit treinrit3 = Treinrit(&nodeE, &nodeH, 2);
+    Treinrit treinrit4 = Treinrit(&nodeF, &nodeD, 4);
+    Treinrit treinrit5 = Treinrit(&nodeF, &nodeG, 5);
+    Vlucht vlucht5 = Vlucht(&nodeH, &nodeE, 6);
+    Autorit autorit5 = Autorit(&nodeH, &nodeG, 5);
+    Autorit autorit6 = Autorit(&nodeG, &nodeH, 1);
 
-    nodeA.edges.insert(nodeA.edges.end(), {&edge1, &edge2, &edge3});
-    nodeB.edges.insert(nodeB.edges.end(), {&edge4});
-    nodeC.edges.insert(nodeC.edges.end(), {&edge5, &edge6, &edge7});
-    nodeD.edges.insert(nodeD.edges.end(), {&edge8, &edge9});
-    nodeE.edges.insert(nodeE.edges.end(), {&edge10, &edge11});
-    nodeF.edges.insert(nodeF.edges.end(), {&edge12, &edge13});
-    nodeG.edges.insert(nodeG.edges.end(), {&edge16});
-    nodeH.edges.insert(nodeH.edges.end(), {&edge14, &edge15});
+    nodeA.edges.insert(nodeA.edges.end(), {&autorit1, &autorit2, &treinrit1});
+    nodeB.edges.insert(nodeB.edges.end(), {&treinrit2});
+    nodeC.edges.insert(nodeC.edges.end(), {&vlucht1, &autorit3, &autorit4});
+    nodeD.edges.insert(nodeD.edges.end(), {&vlucht2, &vlucht3});
+    nodeE.edges.insert(nodeE.edges.end(), {&vlucht4, &treinrit3});
+    nodeF.edges.insert(nodeF.edges.end(), {&treinrit4, &treinrit5});
+    nodeG.edges.insert(nodeG.edges.end(), {&autorit6});
+    nodeH.edges.insert(nodeH.edges.end(), {&vlucht5, &autorit5});
 
     std::vector<Node*> nodes = {&nodeA, &nodeB, &nodeC, &nodeD, &nodeE, &nodeF, &nodeG, &nodeH};
-    std::vector<Edge*> edges = {&edge1, &edge2, &edge3, &edge4, &edge5, &edge6, &edge7, &edge8, 
-                                &edge9, &edge10, &edge11, &edge12, &edge13, &edge14, &edge15, &edge16};
+    std::vector<Edge*> edges = {&autorit1, &autorit2, &treinrit1, &treinrit2, &vlucht1, &autorit3, &autorit4, &vlucht2, 
+                                &vlucht3, &vlucht4, &treinrit3, &treinrit4, &treinrit5, &vlucht5, &autorit5, &autorit6};
 
     Graph graph1 = Graph(nodes, edges);
 
